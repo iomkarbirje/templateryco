0e4738d2efd87db97db0b7bb943b3f119bf6b90c
Filip Stollár <stollarf@gmail.com>
2020-08-31 09:05:40 +0200

    Add thank you page after you hit subscribe

---
+++ src/pages/thanks.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "components/Layout"
+
+const ThankYouPage = () => (
+  <Layout>
+    <h1>You are subscribed!</h1>
+    <p>New templates will now land in your inbox, stay tuned.</p>
+  </Layout>
+)
+
+export default ThankYouPage
