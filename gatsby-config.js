478d52608d90d6671f807a907e2b56463316b645
iomkarbirje <74087734+iomkarbirje@users.noreply.github.com>
2021-05-29 15:47:13 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: "gatsby-source-prismic-graphql",
       options: {
-        repositoryName: "templatery", // (REQUIRED, replace with your own)
+        repositoryName: "iomkarbirje", // (REQUIRED, replace with your own)
         linkResolver: () => post => `/${post.uid}`,
       },
     },
